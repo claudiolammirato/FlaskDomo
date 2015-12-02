a98727a0220463f7a56f4936d21406fa4d056161
Claudio <claudio.lammirato@gmail.com>
2015-12-02 22:14:35 +0100

    0.1
    Structure

--- run.py
+++ run.py
@@ -2,7 +2,7 @@
 from app import create_app, db
 from app.models import User
 
-if __name__ = '__main__':
+if __name__ == '__main__':
     app = create_app('development')
     with app.app_context():
         db.create_all()
